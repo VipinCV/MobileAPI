c77b83933f00ac2333c268de73c7c61449a1d208
Vipin <vipin@vxceed.com>
2025-04-01 16:59:48 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+ //builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
 
 
 // Add services to the container.
@@ -12,9 +21,19 @@ var app = builder.Build();
 
 //if (app.Environment.IsDevelopment() || app.Environment.IsProduction() )
 //{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
+app.UseSwaggerUI();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
+
+//app.UseSwaggerUI(c =>
+//{
+//    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+//});
+////}
 
 // Configure the HTTP request pipeline.
 
