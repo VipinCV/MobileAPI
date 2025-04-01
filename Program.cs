e92ca9cb883d88f4eb7861f3adf0b728056ab1c5
Vipin <vipin@vxceed.com>
2025-04-01 16:05:22 +0530

    swagger

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 // Add services to the container.
 
 var app = builder.Build();
+
+//if (app.Environment.IsDevelopment() || app.Environment.IsProduction() )
+//{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+//}
 
 // Configure the HTTP request pipeline.
 
@@ -26,9 +38,45 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 });
 
+app.MapPost("/todoupdate", (Todo todo) => {
+    if (todo is not null)
+    {
+        todo.Name = todo.NameField;
+    }
+    return todo;
+});
+
+
+app.MapGet("/MobileData", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new MobileData
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
+
 app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+
+internal record MobileData(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+
+class Todo
+{
+    public string? Name { get; set; }
+    public string? NameField;
+    public bool IsComplete { get; set; }
 }
