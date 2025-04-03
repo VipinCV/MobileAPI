0b4e00b3a9ba2ec567e8fb0970898f2621a5a737
Vipin <vipin@vxceed.com>
2025-04-03 22:15:51 +0530

    abortConnect=false

--- Common/RedisHelper.cs
+++ Common/RedisHelper.cs
@@ -5,7 +5,7 @@ public class RedisHelper
 {
     private static readonly Lazy<ConnectionMultiplexer> LazyConnection =
         new Lazy<ConnectionMultiplexer>(() =>
-            ConnectionMultiplexer.Connect("redis://red-cvm0paq4d50c73ftt2pg:6379")); // Change to your Render Redis URL
+            ConnectionMultiplexer.Connect("redis://red-cvm0paq4d50c73ftt2pg:6379,abortConnect=false")); // Change to your Render Redis URL
 
     public static ConnectionMultiplexer Connection => LazyConnection.Value;
 }
