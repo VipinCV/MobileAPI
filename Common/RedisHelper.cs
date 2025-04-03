312509887e5bc6627fcc01d2b363abd72793f2bd
Vipin <vipin@vxceed.com>
2025-04-03 17:55:55 +0530

    commit Redis

---
+++ Common/RedisHelper.cs
@@ -0,0 +1,11 @@
+﻿using StackExchange.Redis;
+using System;
+
+public class RedisHelper
+{
+    private static readonly Lazy<ConnectionMultiplexer> LazyConnection =
+        new Lazy<ConnectionMultiplexer>(() =>
+            ConnectionMultiplexer.Connect("redis://red-cvm0paq4d50c73ftt2pg:6379")); // Change to your Render Redis URL
+
+    public static ConnectionMultiplexer Connection => LazyConnection.Value;
+}
---
+++ Controller/RedisController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Threading.Tasks;
+
+namespace MobileAPI.Controller
+{
+    [Route("api/redis")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+        private readonly IConnectionMultiplexer _redis;
+
+        public RedisController()
+        {
+            _redis = RedisHelper.Connection;
+        }
+
+        [HttpPost("publish")]
+        public async Task<IActionResult> PublishMessage([FromBody] string message)
+        {
+            var pub = _redis.GetSubscriber();
+            await pub.PublishAsync("my_channel", message);
+            return Ok(new { Message = "Published: " + message });
+        }
+    }
+
+}
